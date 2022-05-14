9b55d3e987f892e434f2b294d532c766d9f30c8c
Manjunath <sreerammanjunath1392@gmail.com>
2022-05-14 15:19:48 +0530

    FlyAir

--- pages/index.js
+++ pages/index.js
@@ -33,9 +33,36 @@ export default function Home() {
         </div>
 
         <div className={styles.links}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="30"
+            fill="currentColor"
+            class="bi bi-box-arrow-in-right"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"
+            />
+            <path
+              fill-rule="evenodd"
+              d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
+            />
+          </svg>
           <Link href="/">
-            <a>Home</a>
+            <a>Login</a>
           </Link>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="30"
+            fill="currentColor"
+            class="bi bi-headset"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 1a5 5 0 0 0-5 5v1h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V6a6 6 0 1 1 12 0v6a2.5 2.5 0 0 1-2.5 2.5H9.366a1 1 0 0 1-.866.5h-1a1 1 0 1 1 0-2h1a1 1 0 0 1 .866.5H11.5A1.5 1.5 0 0 0 13 12h-1a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1h1V6a5 5 0 0 0-5-5z" />
+          </svg>
           <Link href="#">
             <a>Contact</a>
           </Link>
